61d36bd813d289648b7e6def2dbe359fbd7e1341
Christian Lobmeier <pardinensis@gmail.com>
2014-05-30 16:16:48 +0200

    error function

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@
 #include <chrono>
 #include <thread>
 
+static void error(const std::string& msg) {
+	std::cerr << "ERROR: " << msg << std::endl;
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char** argv) {
 	if (!glfwInit())
-		exit(EXIT_FAILURE);
+		error("glfwInit failed");
 
 	GLFWwindow* main_window = glfwCreateWindow(640, 480, "dim4", NULL, NULL);
+	if (!main_window) {
+		glfwTerminate();
+		error("glfwCreateWindow failed");
+	}
 
 	std::this_thread::sleep_for(std::chrono::seconds(3));
 	
